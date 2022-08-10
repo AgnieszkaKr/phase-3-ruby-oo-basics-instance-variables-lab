8ae04a1949f57c3dbb714c9172b368b4b2a05c27
Agnes <agsawicka@gmail.com>
2022-08-09 22:25:26 -0400

    commit tree e8882a5c14c0ab3d31e4fcc79ade6d6605b854a9

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,14 @@
 class Dog
-
+    #   setter
+    def name=(dog_name)
+        @this_dogs_name = dog_name
+    end
+    # getter
+    def name
+        @this_dogs_name
+    end
 end
+
+dog_one =Dog.new
+dog_one.name = "Lessi"
+dog_one.name
